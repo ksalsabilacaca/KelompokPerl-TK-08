17372b90fa12bb07f10a5508260e8995ae4fd6f4
ksalsabilacaca <salsammm31@gmail.com>
2025-05-14 22:08:35 +0700

    ngubah int main

--- kalkulator.c
+++ kalkulator.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
-// Fungsi penjumlahan
-int tambah(int a, int b); // Akan diisi oleh anggota 1
+float Multiply(float x, float y);
+float Add(float x, float y);
+float Substraction(float Num1, float Num2);
+float Division(float Num1, float Num2);
 
-// Fungsi pengurangan
-int kurang(int a, int b); // Akan diisi oleh anggota 2
+int main(){
+    float Num1, Num2, Result;
+    int choice;
+    printf("Kalkulator Kelompok Perl\n");
+    printf("Angka Pertama: ");
+    scanf("%d", &Num1);
+    printf("Angka Kedua: ");
+    scanf("%d", &Num2);
+    Printf("1. Penjumlahan\n");
+    printf("2. Pengurangan\n");
+    printf("3. Perkalian\n");
+    printf("4. Pembagian\n");
 
-// Fungsi perkalian
-int kali(int a, int b);   // Akan diisi oleh anggota 3
-
-// Fungsi pembagian
-float bagi(int a, int b); // Akan diisi oleh anggota 4
-
-int main() {
-    int x = 10, y = 5;
-
-    printf("Penjumlahan: %d\n", tambah(x, y));
-    printf("Pengurangan: %d\n", kurang(x, y));
-    printf("Perkalian: %d\n", kali(x, y));
-    printf("Pembagian: %.2f\n", bagi(x, y));
-
-    return 0;
+    printf("Pilih (1-4): ");
+    scanf("%d", &choice);
+    switch(choice){
+        case 1: 
+            Result = Add(Num1,Num2);
+        break;
+        case 2: 
+            Result = Substraction(Num1, Num2);
+        break;
+        case 3: 
+            Result = Multiply(Num1, Num2);
+        break;
+        case 4: 
+            Result = Division(Num1, Num2);
+        break;
+        default: 
+            printf("Pilih (1-4): ");
+            scanf("%d", &choice);
+        break;
+    }
+printf("Result: %.2f", Result);
+return 0;
 }
