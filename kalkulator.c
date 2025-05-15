f5112884da16a61ec9a923d742c02c05f0d81b7f
ksalsabilacaca <salsammm31@gmail.com>
2025-05-15 15:37:48 +0700

    versi terbaru ada result

--- kalkulator.c
+++ kalkulator.c
@@ -10,33 +10,38 @@ int main(){
     int choice;
     printf("Kalkulator Kelompok Perl\n");
     printf("Angka Pertama: ");
-    scanf("%d", &Num1);
+    scanf("%f", &Num1);
     printf("Angka Kedua: ");
-    scanf("%d", &Num2);
-    Printf("1. Penjumlahan\n");
-    printf("2. Pengurangan\n");
-    printf("3. Perkalian\n");
-    printf("4. Pembagian\n");
-
-    printf("Pilih (1-4): ");
-    scanf("%d", &choice);
+    scanf("%f", &Num2);
+    do {
+        printf("1. Penjumlahan\n");
+        printf("2. Pengurangan\n");
+        printf("3. Perkalian\n");
+        printf("4. Pembagian\n");
+        printf("Pilih (1-4): ");
+        scanf("%d", &choice);
+        if (choice < 1 || choice > 4) {
+            printf("Pilihan tidak valid. Coba lagi.\n");
+        }
+    } while (choice < 1 || choice > 4);
+    
     switch(choice){
         case 1: 
             Result = Add(Num1,Num2);
-        break;
+            break;
         case 2: 
             Result = Substraction(Num1, Num2);
-        break;
+            break;
         case 3: 
             Result = Multiply(Num1, Num2);
-        break;
+            break;
         case 4: 
             Result = Division(Num1, Num2);
-        break;
+            break;
         default: 
             printf("Pilih (1-4): ");
             scanf("%d", &choice);
-        break;
+            break;
     }
 printf("Result: %.2f", Result);
 return 0;
@@ -44,7 +49,8 @@ return 0;
 
 float Add(float x, float y){
     return x+y;
-    
+} 
+
 float Substraction(float Num1, float Num2)
 {
     return Num1 - Num2;
@@ -52,7 +58,8 @@ float Substraction(float Num1, float Num2)
 
 float Division(float Num1, float Num2) {
     return Num1 / Num2;
-    
+}
+
 float Multiply(float x, float y){
     return x * y;
 }
